4ce12f31a597db46f0aa747f0a53b6b41d5b6377
VitaliyKapaniuk <98012519+VitaliyKapaniuk@users.noreply.github.com>
2023-03-13 22:47:39 +0200

    lang and theme changes

--- catalog/language/uk-ua/common/footer.php
+++ catalog/language/uk-ua/common/footer.php
@@ -14,4 +14,5 @@ $_['text_account'] = 'Особистий Кабінет';
 $_['text_order'] = 'Історія замовлень';
 $_['text_wishlist'] = 'Закладки';
 $_['text_newsletter'] = 'Розсилка';
-$_['text_powered']      = '<a rel="nofollow" href="https://proplat.biz" target="_blank">Розробка та реклама інтернет магазинів на Opencart</a> %s &copy; %s';
+$_['text_powered']      = '<a href="/" rel="nofollow">Всі права захищено.</a> %s &copy; %s';
+$_['text_myaccount']      = 'Мій Профіль';
