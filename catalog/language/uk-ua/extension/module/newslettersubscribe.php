dff78550af3e0d99dbb38841cbee22a445e84ee3
VitaliyKapaniuk <98012519+VitaliyKapaniuk@users.noreply.github.com>
2023-03-27 16:17:41 +0300

    translation

---
+++ catalog/language/uk-ua/extension/module/newslettersubscribe.php
@@ -0,0 +1,22 @@
+<?php
+
+//Fields
+$_['entry_email'] 		 = 'Введіть свій Email';
+$_['entry_name'] 		 = 'Ім\'я';
+
+//Buttons
+$_['entry_button'] 		 = 'Підписатись';
+$_['entry_unbutton'] 	 = 'Відписатись';
+
+//text
+$_['text_subscribe'] 	 = 'Підпишіться тут';
+
+$_['mail_subject']   	 = 'Підписатися на розсилку';
+
+//Messages
+$_['error_invalid'] 	 = 'Помилка: Введіть правильну електронну адресу.';
+$_['subscribe']	    	 = 'Успішно: Ви підписані.';
+$_['unsubscribe'] 	     = 'Успішно : Підписку скасовано.';
+$_['alreadyexist'] 	     = 'Помилка : Email вже існує.';
+$_['notexist'] 	    	 = 'Помилка : Email не існує.';
+$_['entry_show_again']   =  "Більше не показувати це спливаюче вікно";
